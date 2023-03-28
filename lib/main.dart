07751b470f29d910d2eb2757352d3d02045ad495
A-Fawzyy <fawzy3171@gmail.com>
2023-03-28 03:12:00 +0200

    feat: Enable max frames

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'dart:ui' as ui;
+
 import 'package:custom_sliver_app_bar/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_displaymode/flutter_displaymode.dart';
 
-void main() {
+void main() async {
+
   runApp(const App());
+
+  await FlutterDisplayMode.setHighRefreshRate();
+
 }
